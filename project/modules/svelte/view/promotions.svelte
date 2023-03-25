5a151068df509d87acbcac8dd57187e10525e1e5
veD-tnayrB <Brayanmc.contacto@gmail.com>
2023-03-24 23:37:25 -0400

    Feature: Add the svelte view and add the code to the vue view but it doesnt work for a unknow bug

---
+++ project/modules/svelte/view/promotions.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import BeyondSVG from './icons/beyond.svelte';
+	import SvelteSVG from './icons/svelte.svelte';
+</script>
+
+<div class="promotion">
+	<div class="logos">
+		<BeyondSVG />
+		<SvelteSVG />
+	</div>
+
+	<h2>BeyondJS + Svelte</h2>
+</div>
+
+<style lang="scss">
+	.promotion {
+		.logos {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			gap: 1.5rem;
+		}
+
+		h2 {
+			font-size: 2.5rem;
+		}
+	}
+</style>
